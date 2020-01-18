0e476c48781aaeb169885461b1b2586cecd22482
yeG10 <garyye2007@gmail.com>
2020-01-18 17:55:06 -0500

    collapsible, warnings

---
+++ chrome-extension/popup.js
@@ -0,0 +1,13 @@
+var buttons = document.getElementsByClassName("collapse");
+for (let i = 0; i<buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        }
+        else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}

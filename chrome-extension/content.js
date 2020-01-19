a24cdf753294088897cfeca49049b6b2589a2611
Repap <saml.repap@gmail.com>
2020-01-18 19:21:11 -0500

    added warning boxes

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -3,8 +3,17 @@ function getHyperlinks(){
     for(link in hyperlinks){
         console.log(hyperlinks[link])
         hyperlinks[link].style.color = "orange";
-        
+        addWarningBox(hyperlinks[link].parentElement);
+
         
     }
+}
+
+function addWarningBox(parentElement){
+    var textbox = document.createElement("div");
+    textbox.id = "warning-box";
+    textbox.innerHTML = "Warning!";
+    parentElement.appendChild(textbox);
+
 }
 getHyperlinks();
